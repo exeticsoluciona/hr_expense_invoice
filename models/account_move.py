f769747d28b6fc2540d272b8ea5d639d9151b6e5
Rodrigo Fernández <rodrigo@aquih.com>
2021-12-02 17:10:30 +0000

    Mejor usar pantalla para seleccionar empleado de factura

--- models/account_move.py
+++ models/account_move.py
@@ -7,26 +7,26 @@ import logging
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-
-    def crear_gasto(self):
+    
+    def crear_gasto(self, empleado_id):
         Expense = self.env['hr.expense']
         
         for f in self:
-            anteriores = Expense.search([('factura_id','=',f.id)])
-            
-            if len(anteriores) > 0:
-                raise ValidationError('La factura {} ya fue utilizada en otro gasto, no se puede utilizar en otro gasto.'.format(f.name))
-                
-            if not f.invoice_user_id.employee_id:
-                raise ValidationError('Su usuario no tiene un empleado asignado; sin empleado, no se puede crear un gasto.')
-            
             if f.state == 'posted' and f.invoice_date:
+                anteriores = Expense.search([('factura_id','=',f.id)])
+            
+                if len(anteriores) > 0:
+                    raise ValidationError('La factura {} ya fue utilizada en otro gasto, no se puede utilizar en otro gasto.'.format(f.name))
+                
                 productos = self.env['product.product'].search([('can_be_expensed', '=', True), '|', ('company_id', '=', False), ('company_id', '=', f.company_id.id)])
+                
+                if len(productos) == 0:
+                    raise ValidationError('No existe productos para gastos.')
                 
                 values = {
                     'name': f.name,
                     'date': f.invoice_date,
-                    'employee_id': f.invoice_user_id.employee_id.id,
+                    'employee_id': empleado_id,
                     'factura_id': f.id,
                     'product_id': productos[0].id,
                     'unit_amount': 0,
@@ -35,5 +35,15 @@ class AccountMove(models.Model):
                 
                 exp = Expense.create(values);
                 exp._onchange_factura_id()
-
             
+class HRExpenseInvoiceCrearGasto(models.TransientModel):
+    _name = 'hr.expense.invoice.crear_gasto'
+    _description = 'Crear gastos desde factura'
+    
+    empleado_id = fields.Many2one('hr.employee', required=True)
+    
+    def action_crear_gasto(self):
+        active_ids = self.env.context.get('active_ids')
+
+        for f in self.env['account.move'].browse(active_ids):
+            f.crear_gasto(self.empleado_id.id)
